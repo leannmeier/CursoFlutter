bd2ee36da5e73596cd1ffe382702916e2efdf396
Leandro Agustin <leannmeier72@gmail.com>
2024-09-09 00:45:35 -0300

    Imagenes con ListView

--- lib/main.dart
+++ lib/main.dart
@@ -30,10 +30,18 @@ class _InicioState extends State<Inicio>{
       appBar: AppBar(
         title: Text("Mi App"),
       ),
-      body: Center(
-        child: Text("Contenido"),
-        ),
+       body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Image.network("https://st4.depositphotos.com/1130303/37937/v/450/depositphotos_379374964-stock-illustration-supercar-vector-file-format-separate.jpg"),
+            ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Image.network("https://w7.pngwing.com/pngs/592/353/png-transparent-yellow-convertible-coupe-2015-lamborghini-aventador-2013-lamborghini-aventador-car-lamborghini-reventxf3n-yellow-lamborghini-aventador-car-computer-wallpaper-roadster-mode-of-transport.png"),
+          ),
+        ],
+      ),
     );
   }
 }
-
