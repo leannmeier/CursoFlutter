c01bfc6cd90d0275ee4fc7487cdaa047dfbee49c
Leandro Agustin <leannmeier72@gmail.com>
2024-09-12 23:05:30 -0300

    Row: algunas caracteristicas

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -8,10 +8,39 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Feed Cards'), backgroundColor: Colors.amber,),
-      body: Container(margin: const EdgeInsets.only(top:20), width: double.infinity, height: 350 ,decoration: const BoxDecoration(color: Colors.red), child: const Text("Hola"),),
+      appBar: AppBar(
+        title: const Text('Feed Cards'), 
+        backgroundColor: Colors.amber,
+        ),
+      body: Container(
+        margin: const EdgeInsets.only(top:20), 
+        width: double.infinity, 
+        height: 350,
+        decoration: const BoxDecoration(color: Colors.red),
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: const Row(
+            children: [
+              CircleAvatar(
+                child: Text("J"),
+              ), 
+              SizedBox(width: 10,),
+            Text(
+              "José Fernandez", 
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+                fontSize: 15.0,
+                color: Color.fromARGB(255,35,35,35),
+            )),
+          ],
+        ),
+      )),
       backgroundColor: Colors.grey[300],
-      floatingActionButton: FloatingActionButton(onPressed: (){}, child: const Icon(Icons.add),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){}, 
+        child: const Icon(
+          Icons.add),
+      ),
     );
   }
 }
