89ae2f22b9bf787ecadd00fc65d2d9d0c9110f0b
Leandro Agustin <leannmeier72@gmail.com>
2024-09-12 21:31:57 -0300

    Scaffold: algunas funciones

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MiApp());
+void main() => runApp(const MyApp());
 
-class MiApp extends StatelessWidget{
-  const MiApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Mi App",
-      home: Inicio(),
-    );
-  }
-}
-
-class Inicio extends StatefulWidget{
-  Inicio({Key? key}) : super(key: key);
-
-  @override
-  _InicioState createState() => _InicioState();
-}
-
-class _InicioState extends State<Inicio>{
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Filas, Columnas y Botones"),
-      ),
-      body: Center(
-        child: FloatingActionButton.large(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Icon(Icons.access_time),
-              SizedBox(
-                width: 5,
-                ),
-              Text("NOW"),
-            ],
-          ),
-          onPressed: (){
-            var t = DateTime.now()  ;
-            print(t);
-          }
-          ),
+      title: 'Feed Cards',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Feed Cards'), backgroundColor: Colors.amber,),
+        body: const Center(child: Text('Hola Mundo'),),
+        backgroundColor: Colors.grey[300],
+        floatingActionButton: FloatingActionButton(onPressed: (){}, child: Icon(Icons.add),),
       ),
     );
   }
 }
+
 
